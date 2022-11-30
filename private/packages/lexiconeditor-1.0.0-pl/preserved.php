cbe67c32a91a4a15f7dd13b79fe07dc23c15140a
p565417 <p565417@localdomain>
2022-11-30 08:49:26 +0100

    Gitify prod

---
+++ private/packages/lexiconeditor-1.0.0-pl/preserved.php
@@ -0,0 +1,65 @@
+<?php return array (
+  '9da41542e27ecfeb12a27487c62a00f9' => 
+  array (
+    'criteria' => 
+    array (
+      'name' => 'lexiconeditor',
+    ),
+    'object' => 
+    array (
+      'name' => 'lexiconeditor',
+      'path' => '{core_path}components/lexiconeditor/',
+      'assets_path' => '{assets_path}components/lexiconeditor/',
+    ),
+  ),
+  '020f9712ceb311bdadcb5551b3cb417e' => 
+  array (
+    'criteria' => 
+    array (
+      'key' => 'lexiconeditor.namespace',
+    ),
+    'object' => 
+    array (
+      'key' => 'lexiconeditor.namespace',
+      'value' => 'site',
+      'xtype' => 'modx-combo-namespace',
+      'namespace' => 'lexiconeditor',
+      'area' => 'system',
+      'editedon' => '2021-11-03 12:48:40',
+    ),
+  ),
+  '83b56a30fc203aefc5c846822351c47c' => 
+  array (
+    'criteria' => 
+    array (
+      'category' => 'LexikonEditor',
+    ),
+    'object' => 
+    array (
+      'id' => 38,
+      'parent' => 0,
+      'category' => 'LexikonEditor',
+      'rank' => 0,
+    ),
+  ),
+  'c508e3188078b564e1be58383396cc14' => 
+  array (
+    'criteria' => 
+    array (
+      'text' => 'lexiconeditor.menu',
+    ),
+    'object' => 
+    array (
+      'text' => 'lexiconeditor.menu',
+      'parent' => 'components',
+      'action' => 'home',
+      'description' => 'lexiconeditor.menu_desc',
+      'icon' => '',
+      'menuindex' => 0,
+      'params' => '',
+      'handler' => '',
+      'permissions' => '',
+      'namespace' => 'lexiconeditor',
+    ),
+  ),
+);
